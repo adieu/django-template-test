2b8fcffc8b4a325f1aee6b99d08c8090289d8d16
adieu <adieu@adieu.cn>
2012-04-21 22:45:52 +0800

    Initial Commit.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name="django-template-test",
+    version="0.1",
+    license='BSD',
+    description="A basic django template used for testing.",
+    author='Ivan Diao',
+    author_email='adieu@adieu.me',
+    url='http://github.com/adieu/django-template-test',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+)

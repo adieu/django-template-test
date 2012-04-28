89d1616f9e66382488b94b94f4428d72646e4a2c
adieu <adieu@adieu.me>
2012-04-28 14:35:09 +0800

    Added base-sidebar.html template.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-template-test",
-    version="0.1",
+    version="0.1.1",
     license='BSD',
     description="A basic django template used for testing.",
     author='Ivan Diao',
